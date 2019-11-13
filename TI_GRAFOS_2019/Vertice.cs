d8793d79a836f4896998c89d83bcbbea47800c23
Arthur Santos <arthurwesley7@gmail.com>
2019-11-13 01:00:48 -0300

    Definições da classe Vertice criadas. Irei testar uma implementação.

--- TI_GRAFOS_2019/Vertice.cs
+++ TI_GRAFOS_2019/Vertice.cs
@@ -8,8 +8,36 @@ namespace TI_GRAFOS_2019
 {
     class Vertice
     {
+        #region Especificações da classe Vertice
+        /* Basicamente cada vértice corresponde a uma torre,
+         * cada torre deve ter um ID, esse ID deve ser um número binário
+         * que é baseado ao ID do banco.
+         */
+        #endregion
+        private int id;
+        private List<Aresta> arestas;
         private int grau;
-        //afafsefvsdfvrsg
-        
+
+        public Vertice(int _id, List<Aresta> _arestas)
+        {
+            this.id = _id;
+            this.arestas = _arestas;
+            this.CountGrau();
+        }
+
+        public List<Aresta> Arestas
+        {
+            get => arestas;
+            set
+            {
+                arestas = value;
+                this.CountGrau();
+            }
+        }
+
+        private protected void CountGrau()
+        {
+            this.grau = this.arestas.Count;
+        }
     }
 }
